eddc3ee31d66b87d75fc5ead9a39542e5364acc1
0xDEADC0DE <reaperfanz@yahoo.com>
2018-06-29 00:47:51 +0200

    Update MainForm.cs

--- TypeWriter/MainForm.cs
+++ TypeWriter/MainForm.cs
@@ -15,23 +15,25 @@ namespace TypeWriter
 	public partial class MainForm : Form
 	{
 		IntPtr Chr;
-	[DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+		
+		[DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
 		public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-	[DllImport("USER32.DLL")]
+		[DllImport("USER32.DLL")]
 		public static extern bool SetForegroundWindow(IntPtr hWnd);
     
 		public MainForm()
 		{
 			
 			InitializeComponent();
-			try {
-					string Browser = "Chrome_WidgetWin_1";
-					string Tab = "TypeRacer - Test your typing speed and learn to type faster. Free typing game and competition. Way more fun than a typing tutor! - Google Chrome";
-					 Chr = FindWindow(Browser,Tab);
+			try 
+			{
+				string Browser = "Chrome_WidgetWin_1";
+				string Tab = "TypeRacer - Test your typing speed and learn to type faster. Free typing game and competition. Way more fun than a typing tutor! - Google Chrome";
+				Chr = FindWindow(Browser,Tab);
 			
 			}	
-			catch (Exception exp) {
-				
+			catch (Exception exp)
+			{
 				MessageBox.Show(exp.ToString() + " Nyisd meg a typreracert!");
 				Application.Exit();
 			}
@@ -39,18 +41,19 @@ namespace TypeWriter
 		}
 		void Button1Click(object sender, EventArgs e)
 		{
-			try {
-				String text = textBox1.Text.ToString();
-			char[] chars = szoveg.ToCharArray();
-			SetForegroundWindow(Chr);
-			foreach (var element in chars)
+			try 
 			{
+				String text = textBox1.Text.ToString();
+				char[] chars = szoveg.ToCharArray();
+				SetForegroundWindow(Chr);
+				foreach (var element in chars)
+				{
 				SendKeys.Send(element.ToString());
 				System.Threading.Thread.Sleep((int)numericUpDown1.Value);
-			}
-			textBox1.Text ="";
+				}
+				textBox1.Text ="";
 					
-				} 
+			 } 
 			catch (Exception exp) 
 			{
 				
